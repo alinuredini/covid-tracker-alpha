f392613767556933e8fec8ed71aa734cd193fa06
Alija Nuredini <nuredinialia@gmail.com>
2020-11-21 19:15:40 +0100

    Start

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import SearchCountry from './components/SearchCountry'
 class App extends React.Component {
   render() {
     return (
+      <div className="">
       <div className="container">
         <h1 className='title'>Covid 19 Tracker App</h1>
         <SearchCountry />
-        <strong><a href="https://neupanedipendra.com.np/simple-covid-19-tracker-app-with-react-js/" >See Full Documentation at my blog</a></strong>
+      </div>
       </div>
     );
   }
